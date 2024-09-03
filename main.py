83cbc63c2e090c6d8eb4e45adc2959208c3a28a1
euanbagguley <ebagguley1@gmail.com>
2024-09-03 20:14:28 +0100

    the questions are now parsed into objects with the question string, the answers string and the correct anser.

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import random
 
 def parser(filename): 
     with open(filename) as file: 
@@ -12,10 +13,15 @@ def parser(filename):
         return questions
 
 
+def question_former(questions):
+    question_indexes = random.sample(range(0, len(questions)), 5)
+    question_list = [questions[index] for index in question_indexes]
+    print(question_list)
 
 def main():
     questions = parser('level1_questions.txt')
-    print(questions)
+    question_former(questions)
+
 
 
 
